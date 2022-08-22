2c0217ab42f520c75a59d79a58561243ca763ecf
Kenia Balestra <kenia.balestra@gmail.com>
2022-08-22 19:36:21 +0000

    head function which returns the first item in the array

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  if (array.length === 0) {
+    return undefined;
+  } else {
+    return array[0];
+  }
+};
+
+//TEST CODE
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Cake"]), "Cake");
+assertEqual(head([]), 0);
